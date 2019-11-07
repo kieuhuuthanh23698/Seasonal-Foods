76117f7a34c11598441f2f28c22afc080f92b763
Kiều Hữu Thành <34344168+kieuhuuthanh23698@users.noreply.github.com>
2019-11-07 17:02:58 +0700

    .......

---
+++ SeasonalFoods/app/src/main/java/com/example/acer_pc/seasonalfoods/Activity/signUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.acer_pc.seasonalfoods.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.acer_pc.seasonalfoods.R;
+
+public class signUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
